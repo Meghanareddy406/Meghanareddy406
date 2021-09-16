24c17ac59176de4fce3c75b2579ce6b6e4891a08
Meghanareddy406 <meghana.limbayyagari@popcornapps.com>
2021-09-16 14:18:57 +0530

    removed link

---
+++ pages/api/[...nextauth].js
@@ -0,0 +1,32 @@
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+import { useSession, signIn, signOut } from "next-auth/react"
+
+export default function Component() {
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  )
+}
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    // ...add more providers here
+  ],
+})
